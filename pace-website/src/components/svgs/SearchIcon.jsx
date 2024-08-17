1b53d7a346d6e824fa151bbc091718a335ba0681
Tarun Rajpurohit <87821489+TarunRajpurohit123@users.noreply.github.com>
2024-08-17 12:19:34 +0530

    search svg dynamic

--- pace-website/src/components/svgs/SearchIcon.jsx
+++ pace-website/src/components/svgs/SearchIcon.jsx
@@ -1,7 +1,43 @@
-export default function SearchIcon() {
+export default function SearchIcon({
+  width = "24",
+  height = "24",
+  viewBox = "0 0 24 24",
+  svgFill = "none",
+  color = "var(--pure)",
+  strokeWidth = "1.5",
+  strokeLinecap = "round",
+  strokeLinejoin = "round",
+  classNames,
+  id,
+  styles,
+}) {
   return (
     <>
-      <div>SearchIcon</div>
+      <svg
+        width={width}
+        height={height}
+        viewBox={viewBox}
+        fill={svgFill}
+        className={classNames}
+        id={id}
+        style={styles}
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M11.5 21C16.7467 21 21 16.7467 21 11.5C21 6.25329 16.7467 2 11.5 2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5 21Z"
+          stroke={color}
+          stroke-width={strokeWidth}
+          stroke-linecap={strokeLinecap}
+          stroke-linejoin={strokeLinejoin}
+        />
+        <path
+          d="M22 22L20 20"
+          stroke={color}
+          stroke-width={strokeWidth}
+          stroke-linecap={strokeLinecap}
+          stroke-linejoin={strokeLinejoin}
+        />
+      </svg>
     </>
   );
 }
