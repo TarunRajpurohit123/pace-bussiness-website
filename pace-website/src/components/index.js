1b51de5e963b00f9fd0818c827a79513a4393f30
Tarun Rajpurohit <87821489+TarunRajpurohit123@users.noreply.github.com>
2024-08-17 12:45:21 +0530

    create component themeswitcher and export new component from index.js file

--- pace-website/src/components/index.js
+++ pace-website/src/components/index.js
@@ -4,6 +4,20 @@ import Annoucebar from "./Annoucebar/Announcebar";
 import Footer from "./Footer/Footer";
 import Logo from "./Logo/Logo";
 import SearchIcon from "./svgs/SearchIcon";
+import PhoneIcon from "./svgs/PhoneIcon";
+import MessageIcon from "./svgs/MessageIcon";
 import Hero from "./Hero/Hero";
+import TSwitcher from "./TSwitcher/TSwitcher";
 
-export { Navbar, Button, Annoucebar, Footer, Logo, SearchIcon, Hero };
+export {
+  Navbar,
+  Button,
+  Annoucebar,
+  Footer,
+  Logo,
+  SearchIcon,
+  PhoneIcon,
+  MessageIcon,
+  Hero,
+  TSwitcher,
+};
