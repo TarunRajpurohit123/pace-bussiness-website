2e6b8782b3e3383cfb4764aecd369940941d4702
Tarun Rajpurohit <87821489+TarunRajpurohit123@users.noreply.github.com>
2024-08-17 12:42:09 +0530

    messageIcon dynamic

---
+++ pace-website/src/components/svgs/MessageIcon.jsx
@@ -0,0 +1,46 @@
+export default function MessageIcon(
+  width = "24",
+  height = "25",
+  viewBox = "0 0 24 25",
+  svgFill = "none",
+  color = "var(--primary-text)",
+  strokeLinecap = "round",
+  strokeLinejoin = "round",
+  strokeWidth = "1.5",
+  strokeMiterlimit = "10",
+  styles,
+  classNames,
+  id
+) {
+  return (
+    <>
+      <svg
+        width={width}
+        height={height}
+        viewBox={viewBox}
+        fill={svgFill}
+        xmlns="http://www.w3.org/2000/svg"
+        id={id}
+        className={classNames}
+        style={styles}
+      >
+        <path
+          d="M17 21H7C4 21 2 19.5 2 16V9C2 5.5 4 4 7 4H17C20 4 22 5.5 22 9V16C22 19.5 20 21 17 21Z"
+          stroke={color}
+          stroke-width={strokeWidth}
+          stroke-miterlimit={strokeMiterlimit}
+          stroke-linecap={strokeLinecap}
+          stroke-linejoin={strokeLinejoin}
+        />
+        <path
+          d="M17 9.5L13.87 12C12.84 12.82 11.15 12.82 10.12 12L7 9.5"
+          stroke={color}
+          stroke-width={strokeWidth}
+          stroke-miterlimit={strokeMiterlimit}
+          stroke-linecap={strokeLinecap}
+          stroke-linejoin={strokeLinejoin}
+        />
+      </svg>
+    </>
+  );
+}
