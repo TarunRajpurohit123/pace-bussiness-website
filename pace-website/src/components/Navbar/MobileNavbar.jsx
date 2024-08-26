c24403798e3819c386c253e33377174785ed627c
Tarun Rajpurohit <87821489+TarunRajpurohit123@users.noreply.github.com>
2024-08-26 11:09:24 +0530

    working on about page responsive 1
    mobile

--- pace-website/src/components/Navbar/MobileNavbar.jsx
+++ pace-website/src/components/Navbar/MobileNavbar.jsx
@@ -15,10 +15,10 @@ export default function MobileNavbar() {
   return (
     <>
       <header style={{ background: "var(--mode-bg)" }}>
-        <nav class="px-4 pt-2.5 pb-[3rem]" id="mobile__navbar">
-          <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+        <nav className="px-4 pt-2.5 pb-[3rem]" id="mobile__navbar">
+          <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
             <Logo />
-            <div class="flex items-center lg:order-2">
+            <div className="flex items-center lg:order-2">
               {/* search icon start */}
               <SearchIcon />
               {/* search icon end */}
@@ -26,66 +26,66 @@ export default function MobileNavbar() {
               <TSwitcher />
 
               <button
-                data-collapse-toggle="mobile-menu-2"
+                dataCollapseToggle="mobile-menu-2"
                 type="button"
                 style={{
                   color: "var(--primary-text)",
                   border: "2px solid var(--primary-text)",
                   marginLeft: "1rem",
                 }}
-                class="inline-flex items-center p-2 ml-1 rounded-lg lg:hidden"
-                aria-controls="mobile-menu-2"
-                aria-expanded="false"
+                className="inline-flex items-center p-2 ml-1 rounded-lg lg:hidden"
+                ariaControls="mobile-menu-2"
+                ariaExpanded="false"
                 onClick={() => {
                   isHide === "hidden"
                     ? setIsHide("visible")
                     : setIsHide("hidden");
                 }}
               >
-                <span class="sr-only">Open main menu</span>
+                <span className="sr-only">Open main menu</span>
                 <svg
-                  class="w-10 h-6"
+                  className="w-10 h-6"
                   fill="currentColor"
                   viewBox="0 0 20 20"
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                   ></path>
                 </svg>
                 <svg
-                  class="hidden w-6 h-6"
+                  className="hidden w-6 h-6"
                   fill="currentColor"
                   viewBox="0 0 20 20"
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                   ></path>
                 </svg>
               </button>
             </div>
             <div
-              class={`${isHide} justify-between items-center w-full lg:flex lg:order-1`}
+              className={`${isHide} justify-between items-center w-full lg:flex lg:order-1`}
               style={{
                 background: "var(--mode-bg)",
                 height: "calc(100vh - 13.5rem)",
               }}
               id="mobile-menu-2"
             >
-              <ul class="flex flex-col" style={{ marginTop: "2rem" }}>
-                {links.map((link) => {
+              <ul className="flex flex-col" style={{ marginTop: "2rem" }}>
+                {links?.map((link) => {
                   return (
                     <>
                       <li>
                         <Link
                           href={link?.link}
-                          class="block py-2 pr-4 pl-3 rounded navbar-link"
-                          aria-current="page"
+                          className="block py-2 pr-4 pl-3 rounded navbar-link"
+                          ariaCurrent="page"
                         >
                           {link?.name}
                         </Link>
