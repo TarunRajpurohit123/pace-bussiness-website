9d50182e2f3c565f6d8c44a3520c084ac1a18cc6
Tarun Rajpurohit <87821489+TarunRajpurohit123@users.noreply.github.com>
2024-08-27 20:53:23 +0530

    brand card smooth transition add

--- pace-website/src/app/components/BrandCard.jsx
+++ pace-website/src/app/components/BrandCard.jsx
@@ -1,5 +1,11 @@
+"use client";
+
 import Image from "next/image";
 import { useState } from "react";
+import { Transition } from "react-transition-group";
+import { useRef } from "react";
+
+const duration = 300;
 
 export default function BrandCard({
   brand,
@@ -11,40 +17,76 @@ export default function BrandCard({
   brandLogo,
 }) {
   const [hover, setHover] = useState(false);
+  const nodeRef = useRef(null);
+  const defaultStyle = {
+    transition: `all ${duration}ms ease-in-out`,
+    background: "none",
+  };
+
+  const headingStyle = {
+    transition: `all ${duration}ms ease-in-out`,
+    color: "none",
+  };
+
+  const transitionStyles = {
+    entered: { background: hoverColor },
+    exited: { background: "none" },
+  };
+
+  const transitionStylesHead = {
+    entered: { color: headingColor },
+    exited: { color: "none" },
+  };
   return (
     <>
-      <section
-        className="hbrand-card"
-        style={hover ? { background: hoverColor } : {}}
-        onMouseEnter={() => {
-          setHover(true);
-        }}
-        onMouseLeave={() => {
-          setHover(false);
-        }}
-      >
-        <h1
-          className="hbrand-card--heading"
-          style={hover ? { color: headingColor } : {}}
-        >
-          {brand}
-        </h1>
-        <p className="hbrand-card--paragraph">{content}</p>
-        <div className="relative">
-          <Image
-            src={!hover ? image : hoverImage}
-            width="300"
-            height="300"
-            className="hbrandImage"
-          />
-          {hover && (
-            <div className="brand__overlay flex justify-center items-center flex-col">
-              <h2 className="uppercase brand_overlay_subheading">Discover</h2>
-              <Image src={brandLogo} width="232" height="44" />
+      <Transition nodeRef={nodeRef} in={hover}>
+        {(state) => (
+          <section
+            ref={nodeRef}
+            style={{
+              ...defaultStyle,
+              ...transitionStyles[state],
+            }}
+            className="hbrand-card"
+            // style={hover ? { background: hoverColor } : {}}
+            onMouseEnter={() => {
+              setHover(true);
+            }}
+            onMouseLeave={() => {
+              setHover(false);
+            }}
+          >
+            <h1
+              className="hbrand-card--heading"
+              ref={nodeRef}
+              style={{
+                ...headingStyle,
+                ...transitionStylesHead[state],
+              }}
+              // style={hover ? { color: headingColor } : {}}
+            >
+              {brand}
+            </h1>
+            <p className="hbrand-card--paragraph">{content}</p>
+            <div className="relative">
+              <Image
+                src={!hover ? image : hoverImage}
+                width="300"
+                height="300"
+                className="hbrandImage"
+              />
+              {hover && (
+                <div className="brand__overlay flex justify-center items-center flex-col">
+                  <h2 className="uppercase brand_overlay_subheading">
+                    Discover
+                  </h2>
+                  <Image src={brandLogo} width="232" height="44" />
+                </div>
+              )}
             </div>
-          )}
-        </div>
-      </section>
+          </section>
+        )}
+      </Transition>
     </>
   );
 }
