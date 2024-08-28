05c0ce2509ce289d41c9f9e6191ad076100fb976
Tarun Rajpurohit <87821489+TarunRajpurohit123@users.noreply.github.com>
2024-08-28 18:46:11 +0530

    work on threepillar section

--- pace-website/src/app/components/HomeThreePillar.jsx
+++ pace-website/src/app/components/HomeThreePillar.jsx
@@ -1,117 +1,208 @@
 "use client";
-
-import CircleIcon2 from "@/components/svgs/CircleIcon2";
-import StepLineIcon from "@/components/svgs/StepLineIcon";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Mousewheel, Pagination } from "swiper/modules";
 import Image from "next/image";
+import { Logo } from "@/components";
 
 export default function HomeThreePillar() {
   return (
     <>
       <section className="homeThreePillar page-width pt-[7.5rem] pb-[7.5rem]">
-        {/* three pillar top */}
-        <div className="homeThreePillar__top">
-          <h1 className="homeThreePillar__top__heading">
-            Three <span>Pillars</span>
+        <div className="homeThree__top">
+          <h1 className="htp__heading flex items-center">
+            <Logo classNames={"mr-[1.3rem]"} color={"var(--red)"} />{" "}
+            <span style={{ color: "var(--pure)" }}>Pillars</span>
           </h1>
-          <div className="homeThreePillar__top__content">
-            <p>
-              We source, vet & deploy the top marketing & technology
-              professionals India has to offer. Global companies hire from
-              Uplers to make their hiring faster, easier & cost-effective.
-            </p>
-          </div>
         </div>
-        {/* three pillar main */}
-        <main className="threepillar__main">
-          <div className="threepillar__left">
-            {/* pillar 1 */}
-            <div>
-              <div className="linkLogo">
-                <CircleIcon2 />
-              </div>
-              <div className="linkText">
-                <h2>Inspiring Lifestyle</h2>
-                <p>Fully committed to the success company</p>
-              </div>
-            </div>
-            {/* pillar 1 end */}
 
-            <div className="mb-[1.7rem]">
-              <StepLineIcon />
-            </div>
+        {/* carousel wheelcontrol */}
+        <Swiper
+          direction={"horizontal"}
+          slidesPerView={1}
+          spaceBetween={30}
+          mousewheel={true}
+          pagination={{
+            clickable: true,
+          }}
+          mousewheel={{
+            releaseOnEdges: true, // Enable releaseOnEdges here
+          }}
+          // loop={true}
+          modules={[Mousewheel]}
+          className="mySwiper"
+          onScroll={(e) => {
+            console.log({ e });
+          }}
+        >
+          {/* slide 1 */}
+          <SwiperSlide>
+            <div className="wc_slide flex">
+              <div className="wc__left">
+                <h1>Empowering Dreams</h1>
+                <div className="wc__left__content mt-[2.5rem]">
+                  <h1>What it Represents: </h1>
+                  <p className="mt-[1.5rem]">
+                    The commitment to enhancing the quality of life for
+                    customers through innovative, stylish, and functional
+                    products. This pillar reflects PACE's dedication to creating
+                    brands that resonate with modern lifestyles and consumer
+                    aspirations.
+                  </p>
+                </div>
 
-            {/* pillar 2 */}
-            <div>
-              <div className="linkLogo">
-                <CircleIcon2 />
-              </div>
-              <div className="linkText">
-                <h2>Empowering Dreams</h2>
-                <p>Fully committed to the success company</p>
-              </div>
-            </div>
-            {/* pillar 2 end */}
+                <div className="wc__left__content mt-[1.5rem]">
+                  <h1>Impact: </h1>
+                  <p className="mt-[1.5rem]">
+                    The commitment to enhancing the quality of life for
+                    customers through innovative, stylish, and functional
+                    products. This pillar reflects PACE's dedication to creating
+                    brands that resonate with modern lifestyles and consumer
+                    aspirations.
+                  </p>
+                </div>
 
-            <div className="mb-[1.6rem]">
-              <StepLineIcon />
-            </div>
-
-            {/* pillar 2 */}
-            <div>
-              <div className="linkLogo">
-                <CircleIcon2 />
-              </div>
-              <div className="linkText">
-                <h2>Fulfilling Aspirations</h2>
-                <p>Fully committed to the success company</p>
-              </div>
-            </div>
-            {/* pillar 2 end */}
-          </div>
-
-          {/* three pillar right goes here */}
-          <div className="threepillar__right">
-            <h1>
-              Inspiring <span>Lifestyle</span>
-            </h1>
-            <div className="threeMainContent">
-              {/* first */}
-              <div className="content">
-                <h2>What it Represents: </h2>
-                <p>
-                  The commitment to enhancing the quality of life for customers
-                  through innovative, stylish, and functional products. This
-                  pillar reflects PACE's dedication to creating brands that
-                  resonate with modern lifestyles and consumer aspirations.
-                </p>
-              </div>
-              {/* second */}
-              <div className="content mt-[1.5rem]">
-                <h2>What it Represents: </h2>
-                <p>
-                  The commitment to enhancing the quality of life for customers
-                  through innovative, stylish, and functional products. This
-                  pillar reflects PACE's dedication to creating brands that
-                  resonate with modern lifestyles and consumer aspirations.
-                </p>
-              </div>
-              {/* third */}
-              <div className="content mt-[1.5rem]">
-                <h2>Lifestyle </h2>
-                <div className="mt-[2.5rem] flex items-center">
-                  <Image src="/ostiloslogo2.png" width="137" height="36" />
-                  <Image
-                    src="/cotncandylogo.png"
-                    className="ml-[5rem]"
-                    width="138"
-                    height="36"
-                  />
+                <div className="wc__left__content mt-[1.5rem]">
+                  <h1>Lifestyle: </h1>
+                  <div className="mt-[2.5rem] flex">
+                    <Image
+                      src={"/OstilosLogo.png"}
+                      width={"138"}
+                      height={"36"}
+                    />
+                    <Image
+                      src={"/OstilosLogo.png"}
+                      width={"138"}
+                      height={"36"}
+                      style={{ marginLeft: "5rem" }}
+                    />
+                    <Image
+                      src={"/OstilosLogo.png"}
+                      width={"138"}
+                      height={"36"}
+                      style={{ marginLeft: "5rem" }}
+                    />
+                  </div>
                 </div>
               </div>
+              <div className="wc__right">
+                <Image src={"/01.png"} width={450} height={505} />
+              </div>
             </div>
-          </div>
-          {/* three pillar right end here */}
-        </main>
+          </SwiperSlide>
+          {/* slide 2 */}
+          <SwiperSlide>
+            <div className="wc_slide flex">
+              <div className="wc__left">
+                <h1>Empowering Dreams</h1>
+                <div className="wc__left__content mt-[2.5rem]">
+                  <h1>What it Represents: </h1>
+                  <p className="mt-[1.5rem]">
+                    The commitment to enhancing the quality of life for
+                    customers through innovative, stylish, and functional
+                    products. This pillar reflects PACE's dedication to creating
+                    brands that resonate with modern lifestyles and consumer
+                    aspirations.
+                  </p>
+                </div>
+
+                <div className="wc__left__content mt-[1.5rem]">
+                  <h1>Impact: </h1>
+                  <p className="mt-[1.5rem]">
+                    The commitment to enhancing the quality of life for
+                    customers through innovative, stylish, and functional
+                    products. This pillar reflects PACE's dedication to creating
+                    brands that resonate with modern lifestyles and consumer
+                    aspirations.
+                  </p>
+                </div>
+
+                <div className="wc__left__content mt-[1.5rem]">
+                  <h1>Lifestyle: </h1>
+                  <div className="mt-[2.5rem] flex">
+                    <Image
+                      src={"/OstilosLogo.png"}
+                      width={"138"}
+                      height={"36"}
+                    />
+                    <Image
+                      src={"/OstilosLogo.png"}
+                      width={"138"}
+                      height={"36"}
+                      style={{ marginLeft: "5rem" }}
+                    />
+                    <Image
+                      src={"/OstilosLogo.png"}
+                      width={"138"}
+                      height={"36"}
+                      style={{ marginLeft: "5rem" }}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="wc__right">
+                <Image src={"/03.png"} width={450} height={505} />
+              </div>
+            </div>
+          </SwiperSlide>
+          {/* slide 3 */}
+          <SwiperSlide>
+            <div className="wc_slide flex">
+              <div className="wc__left">
+                <h1>Empowering Dreams</h1>
+                <div className="wc__left__content mt-[2.5rem]">
+                  <h1>What it Represents: </h1>
+                  <p className="mt-[1.5rem]">
+                    The commitment to enhancing the quality of life for
+                    customers through innovative, stylish, and functional
+                    products. This pillar reflects PACE's dedication to creating
+                    brands that resonate with modern lifestyles and consumer
+                    aspirations.
+                  </p>
+                </div>
+
+                <div className="wc__left__content mt-[1.5rem]">
+                  <h1>Impact: </h1>
+                  <p className="mt-[1.5rem]">
+                    The commitment to enhancing the quality of life for
+                    customers through innovative, stylish, and functional
+                    products. This pillar reflects PACE's dedication to creating
+                    brands that resonate with modern lifestyles and consumer
+                    aspirations.
+                  </p>
+                </div>
+
+                <div className="wc__left__content mt-[1.5rem]">
+                  <h1>Lifestyle: </h1>
+                  <div className="mt-[2.5rem] flex">
+                    <Image
+                      src={"/OstilosLogo.png"}
+                      width={"138"}
+                      height={"36"}
+                    />
+                    <Image
+                      src={"/OstilosLogo.png"}
+                      width={"138"}
+                      height={"36"}
+                      style={{ marginLeft: "5rem" }}
+                    />
+                    <Image
+                      src={"/OstilosLogo.png"}
+                      width={"138"}
+                      height={"36"}
+                      style={{ marginLeft: "5rem" }}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="wc__right">
+                <Image src={"/01.png"} width={450} height={505} />
+              </div>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+        {/* end carousel wheelcontrol */}
       </section>
     </>
   );
