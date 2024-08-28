c5deb535c39658112681ae41a48527d8e2bbc81e
Tarun Rajpurohit <87821489+TarunRajpurohit123@users.noreply.github.com>
2024-08-28 14:10:02 +0530

    font change

--- pace-website/src/app/layout.js
+++ pace-website/src/app/layout.js
@@ -1,10 +1,9 @@
-import { Karla } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 import "./tablet-and-tablet.css";
 import { Annoucebar, Footer, Navbar } from "@/components";
 import MobileNavbar from "@/components/Navbar/MobileNavbar";
-
-const inter = Karla({ subsets: ["latin"] });
+import FontProvider from "./providers/fontProvider";
 
 export const metadata = {
   title: "Pace",
@@ -13,16 +12,19 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} dark-mode `}>
-        {/* <Annoucebar /> */}
-        {/* <Navbar /> */}
-        {/* <MobileNavbar /> */}
-        <section className="absolute global-abs top-[5rem] z-[-9]">
-          {children}
-          {/* <Footer /> */}
-        </section>
-      </body>
-    </html>
+    <>
+      <FontProvider />
+      <html lang="en">
+        <body className={`dark-mode`}>
+          {/* <Annoucebar /> */}
+          {/* <Navbar /> */}
+          {/* <MobileNavbar /> */}
+          <section className="absolute global-abs top-[5rem] z-[-9]">
+            {children}
+            {/* <Footer /> */}
+          </section>
+        </body>
+      </html>
+    </>
   );
 }
---
+++ pace-website/src/app/providers/fontProvider.jsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { Poppins, EB_Garamond } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "700"], // Choose the weights you need
+});
+
+const garamonds = EB_Garamond({
+  subsets: ["latin"],
+});
+
+export default function FontProvider() {
+  return (
+    <>
+      <style jsx global>{`
+        p {
+          font-family: ${poppins.style.fontFamily};
+        }
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6 {
+          font-family: ${garamonds.style.fontFamily};
+        }
+      `}</style>
+    </>
+  );
+}
