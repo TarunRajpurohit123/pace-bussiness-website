38e3a49e73a9473cd40cd1b6ff737363c95a3c07
Tarun Rajpurohit <87821489+TarunRajpurohit123@users.noreply.github.com>
2024-08-29 17:34:46 +0530

    update some report related data in js file object

---
+++ pace-website/src/json/reportsData.js
@@ -0,0 +1,93 @@
+const countriesCode = [
+  {
+    id: 1,
+    uniq: "RAP",
+    tab: "Reports & Presentations",
+    tabType: "simple",
+    sidebar: [
+      {
+        type: "General Meeting",
+        data: [
+          { name: "Outcome of AGM", url: "#" },
+          { name: "General Meeting", url: "#" },
+        ],
+      },
+      {
+        type: "Policies",
+        data: [
+          { name: "Whistle Blower Policy", url: "#" },
+          { name: "Related Party Transaction Policy", url: "#" },
+          { name: "Policy on Disclosure of UPSI", url: "#" },
+          { name: "Policy on Board diversity", url: "#" },
+          { name: "Performance Evaluation Policy", url: "#" },
+          { name: "Nomination Remuneration Policy", url: "#" },
+          { name: "Familarization Programme for ID", url: "#" },
+          { name: "Draft Policy on Determing the Material Events", url: "#" },
+          {
+            name: "Code of Internal Procedure Conduct for Prevention of Insider Trading",
+            url: "#",
+          },
+          { name: "Code of conduct", url: "#" },
+          { name: "Code for Independent Director", url: "#" },
+        ],
+      },
+      {
+        type: "Investors Updates",
+        data: [
+          { name: "Pace Investor PitchDeck 2022", url: "#" },
+          { name: "Pace Investor PitchDeck 2021", url: "#" },
+          { name: "Pace - Produce on Demand Video", url: "#" },
+        ],
+      },
+      {
+        type: "Board Meetings",
+        data: [
+          { name: "Intimation of BM", url: "#" },
+          { name: "Outcome of BM", url: "#" },
+        ],
+      },
+      {
+        type: "Annual reports and returns",
+        data: null,
+      },
+    ],
+  },
+  {
+    id: 2,
+    uniq: "ANN",
+    tab: "Announcement",
+    tabType: "mix",
+    sidebar: [
+      {
+        type: "Closure of trading window",
+        data: null,
+      },
+      {
+        type: "SDD",
+        data: null,
+      },
+      {
+        type: "Voting Results",
+        data: null,
+      },
+      {
+        type: "Quarterly Investor Grievances",
+        data: null,
+      },
+      {
+        type: "Disclosures",
+        data: null,
+      },
+      {
+        type: "General Announcements",
+        data: null,
+      },
+    ],
+  },
+  {
+    id: 3,
+    uniq: "SP",
+    tab: "Shareholding Pattern",
+    tabType: "yearOnly",
+  },
+];
