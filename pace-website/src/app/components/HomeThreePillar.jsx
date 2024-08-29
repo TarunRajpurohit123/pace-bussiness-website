1edefd0b42fb17a340c608b180454936fc6f7f34
Tarun Rajpurohit <87821489+TarunRajpurohit123@users.noreply.github.com>
2024-08-29 12:02:43 +0530

    working on home three pillar section

--- pace-website/src/app/components/HomeThreePillar.jsx
+++ pace-website/src/app/components/HomeThreePillar.jsx
@@ -5,21 +5,76 @@ import "swiper/css/pagination";
 import { Mousewheel, Pagination } from "swiper/modules";
 import Image from "next/image";
 import { Logo } from "@/components";
+import DotIcon from "@/components/svgs/dotIcon";
+import { useRef, useState } from "react";
 
 export default function HomeThreePillar() {
+  const [activeDot, setActiveDot] = useState({
+    first: { color: "var(--red)", scale: "scale-150" },
+    second: { color: "var(--gray_400)", scale: "scale-100" },
+    third: { color: "var(--gray_400)", scale: "scale-100" },
+  });
+  const swiperRef = useRef(null);
   return (
     <>
       <section className="homeThreePillar page-width pt-[7.5rem] pb-[7.5rem]">
-        <div className="homeThree__top">
+        <div className="homeThree__top flex items-center justify-between">
           <h1 className="htp__heading flex items-center">
             <Logo classNames={"mr-[1.3rem]"} color={"var(--red)"} />{" "}
             <span style={{ color: "var(--pure)" }}>Pillars</span>
           </h1>
+          <div className="pillarCarouselDots flex">
+            <DotIcon
+              onClick={() => {
+                swiperRef.current.slideTo(0);
+                setActiveDot(() => {
+                  return {
+                    first: { color: "var(--red)", scale: "scale-150" },
+                    second: { color: "var(--gray_400)", scale: "scale-100" },
+                    third: { color: "var(--gray_400)", scale: "scale-100" },
+                  };
+                });
+              }}
+              className={`mr-[1rem] ${activeDot?.first?.scale}`}
+              color={activeDot?.first?.color}
+            />
+            <DotIcon
+              onClick={() => {
+                swiperRef.current.slideTo(2);
+                setActiveDot(() => {
+                  return {
+                    second: { color: "var(--red)", scale: "scale-150" },
+                    first: { color: "var(--gray_400)", scale: "scale-100" },
+                    third: { color: "var(--gray_400)", scale: "scale-100" },
+                  };
+                });
+              }}
+              className={`mr-[1rem] ${activeDot?.second?.scale}`}
+              color={activeDot?.second?.color}
+            />
+            <DotIcon
+              onClick={() => {
+                swiperRef.current.slideTo(1);
+                setActiveDot(() => {
+                  return {
+                    third: { color: "var(--red)", scale: "scale-150" },
+                    second: { color: "var(--gray_400)", scale: "scale-100" },
+                    first: { color: "var(--gray_400)", scale: "scale-100" },
+                  };
+                });
+              }}
+              className={`mr-[1rem] ${activeDot?.third?.scale}`}
+              color={activeDot?.third?.color}
+            />
+          </div>
         </div>
 
         {/* carousel wheelcontrol */}
         <Swiper
           direction={"horizontal"}
+          onSwiper={(swiper) => {
+            swiperRef.current = swiper; // Store the Swiper instance in the ref
+          }}
           slidesPerView={1}
           spaceBetween={30}
           mousewheel={true}
@@ -34,6 +89,36 @@ export default function HomeThreePillar() {
           className="mySwiper"
           onScroll={(e) => {
             console.log({ e });
+          }}
+          onSlideChange={(e) => {
+            console.log("slideChnage", e?.activeIndex);
+            if (e?.activeIndex == 0) {
+              setActiveDot(() => {
+                return {
+                  first: { color: "var(--red)", scale: "scale-150" },
+                  second: { color: "var(--gray_400)", scale: "scale-100" },
+                  third: { color: "var(--gray_400)", scale: "scale-100" },
+                };
+              });
+            }
+            if (e?.activeIndex == 1) {
+              setActiveDot(() => {
+                return {
+                  second: { color: "var(--red)", scale: "scale-150" },
+                  third: { color: "var(--gray_400)", scale: "scale-100" },
+                  first: { color: "var(--gray_400)", scale: "scale-100" },
+                };
+              });
+            }
+            if (e?.activeIndex == 2) {
+              setActiveDot(() => {
+                return {
+                  third: { color: "var(--red)", scale: "scale-150" },
+                  second: { color: "var(--gray_400)", scale: "scale-100" },
+                  first: { color: "var(--gray_400)", scale: "scale-100" },
+                };
+              });
+            }
           }}
         >
           {/* slide 1 */}
---
+++ pace-website/src/components/svgs/dotIcon.jsx
@@ -0,0 +1,32 @@
+export default function DotIcon({
+  color = "var(--gray_400)",
+  width = "8",
+  height = "9",
+  view = `0 0 ${width} ${height}`,
+  className,
+  styles,
+  onClick,
+}) {
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={width}
+        height={height}
+        viewBox={view}
+        fill="none"
+        className={className}
+        style={{ cursor: "pointer" }}
+        onClick={onClick}
+      >
+        <circle
+          cx="4"
+          cy="4.5"
+          r="4"
+          transform="rotate(-180 4 4.5)"
+          fill={color}
+        />
+      </svg>
+    </>
+  );
+}
