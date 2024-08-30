33d1840cfa7ad67f337cd03d179bb2af5a3cc793
Tarun Rajpurohit <87821489+TarunRajpurohit123@users.noreply.github.com>
2024-08-30 17:47:14 +0530

    video bg properly set to home page

--- pace-website/src/components/Navbar/Navbar.jsx
+++ pace-website/src/components/Navbar/Navbar.jsx
@@ -1,35 +1,39 @@
 import Link from "next/link";
 import { Logo, SearchIcon, Spacer } from "..";
 import { links } from "./links";
+import Overlay from "./Overlay";
 
 export default function Navbar() {
   return (
-    <>
-      <Spacer classes={"w-full h-[1rem] mobile-spacer"} />
+    <div className="relative">
+      <Overlay />
+
       <nav
-        className="page-width mt-[1rem] flex justify-between items-center"
+        className="page-width  mt-[1rem] flex justify-between items-center z-50"
         id="desktop__navbar"
       >
-        <Logo />
+        <div className="z-10 flex justify-between w-full">
+          <Logo />
 
-        {/* links goes here*/}
-        <ul className="flex">
-          {links?.map((link, ind) => {
-            return (
-              <li className={ind != 0 ? `ml-[2.5rem]` : ``}>
-                <Link className="navbar-link" href={link?.link}>
-                  {link?.name}
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-        {/* links end here*/}
+          {/* links goes here*/}
+          <ul className="flex">
+            {links?.map((link, ind) => {
+              return (
+                <li className={ind != 0 ? `ml-[2.5rem]` : ``}>
+                  <Link className="navbar-link" href={link?.link}>
+                    {link?.name}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+          {/* links end here*/}
 
-        <div className="cursor-pointer">
-          <SearchIcon />
+          <div className="cursor-pointer">
+            <SearchIcon />
+          </div>
         </div>
       </nav>
-    </>
+    </div>
   );
 }
---
+++ pace-website/src/components/Navbar/Overlay.jsx
@@ -0,0 +1,5 @@
+export default function Overlay() {
+  return (
+    <section className="navbar__overlay absolute top-0 z-0 left-0 w-full"></section>
+  );
+}
