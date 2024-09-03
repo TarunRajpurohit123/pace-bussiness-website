260abea644fb005642ce1ea2079c998163e42adc
Tarun Rajpurohit <87821489+TarunRajpurohit123@users.noreply.github.com>
2024-09-03 15:44:44 +0530

    home overlay set

--- pace-website/src/app/investors/page.jsx
+++ pace-website/src/app/investors/page.jsx
@@ -1,15 +1,15 @@
-import Hero from "../../components/components/Hero";
 import InvestorCTA from "../../components/components/InvestorCTA";
 import FinancialCalender from "../../components/components/FinancialCalender";
 import InvestorStatics from "../../components/components/InvestorStatics";
 import LatestPress from "../../components/components/LatestPress";
 import ReportSection from "@/components/components/ReportSection";
+import Hero2 from "@/components/components/Hero2";
 
 export default function investors() {
   return (
     <>
-      <Hero
-        className="pages_investor_wrapper"
+      <Hero2
+        // className="pages_investor_wrapper"
         title="Investor Relation Highlights"
         content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore."
       />
