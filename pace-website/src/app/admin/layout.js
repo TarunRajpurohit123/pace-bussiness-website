e9cce96517610096dc4015e11c9e50a2c39fe447
Tarun Rajpurohit <87821489+TarunRajpurohit123@users.noreply.github.com>
2024-09-05 12:36:01 +0530

    add admin page

--- pace-website/src/app/admin/layout.js
+++ pace-website/src/app/admin/layout.js
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function AdminLayout({ children }) {
   return (
-    <div className="admin-layout absolute top-[-50px]">
+    <div className="admin-layout w-full h-[100vh]  absolute top-[-50px] flex">
       <aside className="admin__sidebar bg-black w-[20rem] h-[100vh]">
         sidebar
       </aside>
-      <div className="admin-content">
+      <div className="admin-content bg-white w-[119rem]">
         <main>{children}</main>
       </div>
     </div>
