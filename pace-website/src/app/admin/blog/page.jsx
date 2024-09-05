f6d2413a53001f275bda9cf7ceeda6ae6255c902
Tarun Rajpurohit <87821489+TarunRajpurohit123@users.noreply.github.com>
2024-09-05 16:59:58 +0530

    create blog page done

--- pace-website/src/app/admin/blog/page.jsx
+++ pace-website/src/app/admin/blog/page.jsx
@@ -1,16 +1,18 @@
 import Datatable from "@/components/Datatable/Datatable";
+import Link from "next/link";
 
 export default function Blog() {
   return (
     <section className="blogs__wrapper">
       <div className="topper flex justify-between items-center">
         <div style={{ fontSize: "3rem" }}>Blogs</div>
-        <button
+        <Link
+          href="/admin/blog/123"
           className="flex justify-center items-center bg-black text-white px-[1rem] py-[0.2rem] min-w-[10rem] h-[3rem]"
           style={{ fontSize: "1.5rem" }}
         >
           Create Blog
-        </button>
+        </Link>
       </div>
       <div className="mt-[1rem]">
         <Datatable />
