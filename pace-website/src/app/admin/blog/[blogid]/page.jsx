0e74454a6a32cd845a8d655abcd937a3e9c9849b
Tarun Rajpurohit <87821489+TarunRajpurohit123@users.noreply.github.com>
2024-09-05 17:19:24 +0530

    working on blog create form

--- pace-website/src/app/admin/blog/[blogid]/page.jsx
+++ pace-website/src/app/admin/blog/[blogid]/page.jsx
@@ -1,3 +1,39 @@
 export default function BlogCreate() {
-  return <>blog create</>;
+  return (
+    <section className="blogs__wrapper">
+      <div className="topper flex justify-between items-center">
+        <div style={{ fontSize: "3rem" }}>Blog Create</div>
+        <button
+          className="flex justify-center items-center bg-black text-white px-[1rem] py-[0.2rem] min-w-[10rem] h-[3rem]"
+          style={{ fontSize: "1.5rem" }}
+        >
+          Save
+        </button>
+      </div>
+      <div className="mt-[1rem]">
+        <form>
+          <div className="flex flex-col">
+            <label htmlFor="blogtitle" style={{ fontSize: "1.5rem" }}>
+              Blog Title
+            </label>
+            <input
+              className="w-full mt-[0.5rem] p-[1rem] outline-1 border"
+              name="blogtitle"
+              id="blogtitle"
+              placeholder="Blog Title"
+              style={{ fontSize: "1.3rem" }}
+            />
+          </div>
+          <div className="flex">
+            <button
+              className="mt-[1rem] flex justify-center items-center bg-black text-white px-[1rem] py-[0.2rem] min-w-[10rem] h-[3rem]"
+              style={{ fontSize: "1.5rem" }}
+            >
+              Create Blog
+            </button>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
 }
