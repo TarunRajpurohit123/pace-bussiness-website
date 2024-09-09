27641ebf269894d4754529de143a88844c11f8b8
Tarun Rajpurohit <87821489+TarunRajpurohit123@users.noreply.github.com>
2024-09-09 18:00:24 +0530

    back button functionality done

--- pace-website/src/components/components/Hero2.jsx
+++ pace-website/src/components/components/Hero2.jsx
@@ -1,15 +1,30 @@
-import { Button } from "..";
+"use client";
+
+import { usePathname, useRouter } from "next/navigation";
 
 export default function Hero2({
   title = "Our Group",
   content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore",
 }) {
+  const router = useRouter();
+  const currentRoute = usePathname();
   return (
     <>
       <section className="hero__two">
         <h1>{title}</h1>
         <p>{content}</p>
-        <button>Back</button>
+        <button
+          style={
+            currentRoute === "/investors/press-releases"
+              ? { display: "block" }
+              : { display: "none" }
+          }
+          onClick={() => {
+            router?.back();
+          }}
+        >
+          Back
+        </button>
       </section>
     </>
   );
