235e37d4d0e9cb52ac79e92ee85ccfde9cac1684
Tarun Rajpurohit <87821489+TarunRajpurohit123@users.noreply.github.com>
2024-09-10 16:51:48 +0530

    annoucement and ipo

--- pace-website/src/json/reportsData.js
+++ pace-website/src/json/reportsData.js
@@ -1,97 +1,459 @@
 const reportsData = [
   {
     id: 1,
-    uniq: "RAP",
-    tab: "Reports & Presentations",
-    tabType: "simple",
+    uniq: "annoucemnet",
+    tab: "ANNOUNCEMENT",
     sidebar: [
       {
-        type: "General Meeting",
+        type: "CLOSURE OF TRADING WINDOW",
+        years: ["2022-2023", "2023-2024"],
         data: [
-          { name: "Outcome of AGM", url: "#" },
-          { name: "General Meeting", url: "#" },
+          {
+            year: "2022-2023",
+            title: "Closure-of-Trading-Window_30.03.2023",
+            url: "/main/ANNOUNCEMENT/CLOSURE OF TRADING WINDOW/2022-2023/Closure-of-Trading-Window_30.03.2023.pdf",
+          },
+          {
+            year: "2023-2024",
+            title: "Closure-of-Trading-Window_30.09.2023",
+            url: "/main/ANNOUNCEMENT/CLOSURE OF TRADING WINDOW/2023-2024/Closure-of-Trading-Window_30.09.2023.pdf",
+          },
+          {
+            year: "2023-2024",
+            title: "Closure-of-Trading-Window_31-03-2024",
+            url: "/main/ANNOUNCEMENT/CLOSURE OF TRADING WINDOW/2023-2024/Closure-of-Trading-Window_31-03-2024.pdf",
+          },
         ],
       },
       {
-        type: "Policies",
+        type: "DISCLOSURES",
         data: [
-          { name: "Whistle Blower Policy", url: "#" },
-          { name: "Related Party Transaction Policy", url: "#" },
-          { name: "Policy on Disclosure of UPSI", url: "#" },
-          { name: "Policy on Board diversity", url: "#" },
-          { name: "Performance Evaluation Policy", url: "#" },
-          { name: "Nomination Remuneration Policy", url: "#" },
-          { name: "Familarization Programme for ID", url: "#" },
-          { name: "Draft Policy on Determing the Material Events", url: "#" },
           {
-            name: "Code of Internal Procedure Conduct for Prevention of Insider Trading",
+            innerTab: "COMPLIANCE CERTIFICATE",
+            innerData: [
+              {
+                year: "2022-2023",
+                title: "Regulation_74(5)_Compliance Certificate_Q2",
+                url: "#",
+              },
+              {
+                year: "2022-2023",
+                title: "Regulation_74(5)_Compliance Certificate_Q3",
+                url: "#",
+              },
+              {
+                year: "2022-2023",
+                title: "Regulation_74(5)_Compliance Certificate_Q4",
+                url: "#",
+              },
+
+              {
+                year: "2023-2024",
+                title: "Regulation_74(5)_Compliance Certificate_Q1",
+                url: "#",
+              },
+              {
+                year: "2023-2024",
+                title: "Regulation_74(5)_Compliance Certioficate_Q2",
+                url: "#",
+              },
+              {
+                year: "2023-2024",
+                title: "Regulation_74(5)_Compliance Certificate_Q3",
+                url: "#",
+              },
+              {
+                year: "2023-2024",
+                title: "Regulation_74(5)_Compliance Certificate_Q4",
+                url: "#",
+              },
+
+              {
+                year: "2024-2025",
+                title: "Regulation_74(5)_Compliance Certificate_Q1",
+                url: "#",
+              },
+            ],
+          },
+          {
+            innerTab: "CORPORATE GOVERNANCE",
+            innerData: [
+              {
+                year: "2022-2023",
+                title: "Regulation 27(2)_Compliance Certificate_Q3",
+                url: "#",
+              },
+              {
+                year: "2022-2023",
+                title: "Regulation 27(2)_Compliance Certificate_Q4",
+                url: "#",
+              },
+              {
+                year: "2023-2024",
+                title: "Regulation 27(2)_Compliance Certificate_Q1",
+                url: "#",
+              },
+              {
+                year: "2023-2024",
+                title: "Regulation 27(2)_Compliance Certificate_Q2",
+                url: "#",
+              },
+              {
+                year: "2023-2024",
+                title: "Regulation 27(2)_Compliance Certificate_Q3",
+                url: "#",
+              },
+              {
+                year: "2023-2024",
+                title: "Regulation 27(2)_Compliance Certificate_Q4",
+                url: "#",
+              },
+              {
+                year: "2024-2025",
+                title: "Regulation 27(2)_Compliance Certificate_Q1",
+                url: "#",
+              },
+            ],
+          },
+          {
+            innerTab: "GENERAL ANNOUNCEMENTS",
+            innerData: [
+              {
+                year: "2022-2023",
+                title: "Appointment_Internal Auditor_2022-23",
+                url: "#",
+              },
+              {
+                year: "2022-2023",
+                title: "Appointment_Secretarial Auditor_2022-23",
+                url: "#",
+              },
+              {
+                year: "2022-2023",
+                title: "Certficate_Regulation_24A_2022-23",
+                url: "#",
+              },
+              {
+                year: "2022-2023",
+                title: "Certificate_Regulation_7(3)_2022-23",
+                url: "#",
+              },
+              {
+                year: "2022-2023",
+                title: "Certificate_Regulation_40(9)_2022-23",
+                url: "#",
+              },
+              {
+                year: "2022-2023",
+                title: "Declaration_SAST Regulation_31(4)_2022-23",
+                url: "#",
+              },
+              {
+                year: "2022-2023",
+                title:
+                  "Regulation 29(1) of SEBI (SAST) REGULATION_VICKY JAVERI",
+                url: "#",
+              },
+
+              {
+                year: "2023-2024",
+                title: "Appointment_Internal Auditor_2023-24",
+                url: "#",
+              },
+              {
+                year: "2023-2024",
+                title: "Appointment_Secretarial Auditor_2023-24",
+                url: "#",
+              },
+              {
+                year: "2023-2024",
+                title: "Appointment_Statutory Auditor_2023",
+                url: "#",
+              },
+              {
+                year: "2023-2024",
+                title: "Book-Closure_AGM_2023-24",
+                url: "#",
+              },
+              {
+                year: "2023-2024",
+                title: "Certficate_Regulation_24A_2023-24",
+                url: "#",
+              },
+              {
+                year: "2023-2024",
+                title: "Certificate_Regulation_7(3)_2023-24",
+                url: "#",
+              },
+              {
+                year: "2023-2024",
+                title: "Certificate_Regulation_40(9)_2023-24",
+                url: "#",
+              },
+              {
+                year: "2023-2024",
+                title: "Clarification_Price_Movement_2023",
+                url: "#",
+              },
+              {
+                year: "2023-2024",
+                title: "Declaration_SAST Regulation_31(4)_2023-24",
+                url: "#",
+              },
+              {
+                year: "2023-2024",
+                title: "General Announcement_Reg 31_SAST",
+                url: "#",
+              },
+              {
+                year: "2023-2024",
+                title: "Reg 29(2) of SAST_Intimation_05.03.2024",
+                url: "#",
+              },
+              {
+                year: "2023-2024",
+                title: "Reg 29(2) of SAST_Intimation_06.03.2024",
+                url: "#",
+              },
+              {
+                year: "2023-2024",
+                title: "Reg 29(2) of SAST_Intimation_15.03.2024",
+                url: "#",
+              },
+              {
+                year: "2023-2024",
+                title: "Reg 29(2) of SAST_Intimation_22.02.2024",
+                url: "#",
+              },
+              {
+                year: "2023-2024",
+                title: "Reg 29(2) of SAST_Intimation_23.02.2024",
+                url: "#",
+              },
+              {
+                year: "2023-2024",
+                title: "Regulation 7(2)_PIT Regulation_05.03.2024",
+                url: "#",
+              },
+              {
+                year: "2023-2024",
+                title: "Regulation 7(2)_PIT Regulation_06.03.2024",
+                url: "#",
+              },
+              {
+                year: "2023-2024",
+                title: "Regulation 7(2)_PIT Regulation_15.03.2024",
+                url: "#",
+              },
+              {
+                year: "2023-2024",
+                title: "Regulation 7(2)_PIT Regulation_22.02.2024",
+                url: "#",
+              },
+              {
+                year: "2023-2024",
+                title: "Regulation 7(2)_PIT Regulation_23.02.2024",
+                url: "#",
+              },
+              {
+                year: "2023-2024",
+                title: "Scrutinizer-Report_AGM_2023-24",
+                url: "#",
+              },
+
+              {
+                year: "2024-2025",
+                title: "Appointment_Internal Auditor_2023-24",
+                url: "#",
+              },
+              {
+                year: "2024-2025",
+                title: "Appointment_Secretarial Auditor_2023-24",
+                url: "#",
+              },
+              {
+                year: "2024-2025",
+                title: "Book-Closure_AGM_2024-25",
+                url: "#",
+              },
+              {
+                year: "2024-2025",
+                title: "Intimation_Regulation 31(1)_ SAST_",
+                url: "#",
+              },
+              {
+                year: "2024-2025",
+                title: "Reg 29(2) of SAST_Intimation_13.08.2024",
+                url: "#",
+              },
+              {
+                year: "2024-2025",
+                title: "Regulation 7(2)_PIT Regulation_30.08.2024",
+                url: "#",
+              },
+            ],
+          },
+          {
+            innerTab: "RECONCILIATION OF SHARE CAPITAL",
+            innerData: [
+              {
+                year: "2022-2023",
+                title: "Reconciliation_Share Capital_Q3",
+                url: "#",
+              },
+              {
+                year: "2022-2023",
+                title: "Reconciliation_Share Capital_Q4",
+                url: "#",
+              },
+
+              {
+                year: "2023-2024",
+                title: "Reconciliation_Share Capital_Q1",
+                url: "#",
+              },
+              {
+                year: "2023-2024",
+                title: "Reconciliation_Share Capital_Q2",
+                url: "#",
+              },
+              {
+                year: "2023-2024",
+                title: "Reconciliation_Share Capital_Q3",
+                url: "#",
+              },
+              {
+                year: "2023-2024",
+                title: "Reconciliation_Share Capital_Q4",
+                url: "#",
+              },
+
+              {
+                year: "2024-2025",
+                title: "Reconciliation_Share Capital_Q1",
+                url: "#",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        type: "QUARTERLY INVESTOR GRIEVANCES",
+        years: ["2022-2023", "2023-2024", "2024-2025"],
+        data: [
+          {
+            year: "2022-2023",
+            title: "INVESTOR GRIVENCE_2022-23_Q2",
             url: "#",
           },
-          { name: "Code of conduct", url: "#" },
-          { name: "Code for Independent Director", url: "#" },
+          {
+            year: "2022-2023",
+            title: "INVESTOR GRIVENCE_2022-23_Q3",
+            url: "#",
+          },
+          {
+            year: "2022-2023",
+            title: "INVESTOR GRIVENCE_2022-23_Q4",
+            url: "#",
+          },
+
+          {
+            year: "2023-2024",
+            title: "INVESTORS GRIVENCES_2023-2024_Q1",
+            url: "#",
+          },
+          {
+            year: "2023-2024",
+            title: "INVESTORS GRIVENCES_2023-2024_Q2",
+            url: "#",
+          },
+          {
+            year: "2023-2024",
+            title: "INVESTORS GRIVENCES_2023-2024_Q3",
+            url: "#",
+          },
+          {
+            year: "2023-2024",
+            title: "INVESTORS GRIVENCES_2023-2024_Q4",
+            url: "#",
+          },
+
+          {
+            year: "2024-2025",
+            title: "INVESTORS GRIVENCES_2024-2025_Q1",
+            url: "#",
+          },
         ],
       },
       {
-        type: "Investors Updates",
+        type: "SDD",
+        years: ["2022-2023", "2023-2024", "2024-2025"],
         data: [
-          { name: "Pace Investor PitchDeck 2022", url: "#" },
-          { name: "Pace Investor PitchDeck 2021", url: "#" },
-          { name: "Pace - Produce on Demand Video", url: "#" },
+          {
+            year: "2022-2023",
+            title: "SDD_Q4_2022-23",
+            url: "#",
+          },
+
+          {
+            year: "2023-2024",
+            title: "SDD Compliance_Q1",
+            url: "#",
+          },
+          {
+            year: "2023-2024",
+            title: "SDD Compliance_Q2",
+            url: "#",
+          },
+          {
+            year: "2023-2024",
+            title: "SDD Compliance_Q3",
+            url: "#",
+          },
+          {
+            year: "2023-2024",
+            title: "SDD Compliance_Q4",
+            url: "#",
+          },
+
+          {
+            year: "2024-2025",
+            title: "SDD Compliance_Q1",
+            url: "#",
+          },
         ],
       },
       {
-        type: "Board Meetings",
+        type: "VOTING RESULT",
+        years: ["2023-2024"],
         data: [
-          { name: "Intimation of BM", url: "#" },
-          { name: "Outcome of BM", url: "#" },
+          {
+            year: "2023-2024",
+            title: "Scrutinizer_Report_",
+            url: "#",
+          },
         ],
-      },
-      {
-        type: "Annual reports and returns",
-        data: null,
       },
     ],
   },
   {
     id: 2,
-    uniq: "ANN",
-    tab: "Announcement",
-    tabType: "mix",
+    uniq: "ipo",
+    tab: "INITIAL PUBLIC OFFERING",
     sidebar: [
       {
-        type: "Closure of trading window",
-        data: [
-          { name: "demo", url: "#", year: 2021 },
-          { name: "demo", url: "#", year: 2022 },
-        ],
+        name: "Draft Prospectus",
+        url: "#",
       },
       {
-        type: "SDD",
-        data: null,
+        name: "General Information Document",
+        url: "#",
       },
       {
-        type: "Voting Results",
-        data: null,
-      },
-      {
-        type: "Quarterly Investor Grievances",
-        data: null,
-      },
-      {
-        type: "Disclosures",
-        data: null,
-      },
-      {
-        type: "General Announcements",
-        data: null,
+        name: "Prospectus",
+        url: "#",
       },
     ],
-  },
-  {
-    id: 3,
-    uniq: "SP",
-    tab: "Shareholding Pattern",
-    tabType: "yearOnly",
   },
 ];
 
