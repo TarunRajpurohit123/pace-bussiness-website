5e723bed75302d7b80ab025b2b72e82ed1178517
Tarun Rajpurohit <87821489+TarunRajpurohit123@users.noreply.github.com>
2024-09-23 12:15:44 +0530

    annoucebar,navbar,video done for 1000

--- pace-website/src/components/Navbar/MobileNavbar.jsx
+++ pace-website/src/components/Navbar/MobileNavbar.jsx
@@ -44,15 +44,28 @@ export default function MobileNavbar() {
                 }}
               >
                 <span className="sr-only">Open main menu</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="24px"
-                  viewBox="0 -960 960 960"
-                  width="24px"
-                  fill="var(--pure)"
-                >
-                  <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
-                </svg>
+
+                {isHide === "hidden" ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="24px"
+                    viewBox="0 -960 960 960"
+                    width="24px"
+                    fill="var(--pure)"
+                  >
+                    <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="24px"
+                    viewBox="0 -960 960 960"
+                    width="24px"
+                    fill="var(--pure)"
+                  >
+                    <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+                  </svg>
+                )}
               </button>
             </div>
             <div
