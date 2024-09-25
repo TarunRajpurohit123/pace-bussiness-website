81d29fd2472223b8515351645fa72984fea877cd
Tarun Rajpurohit <87821489+TarunRajpurohit123@users.noreply.github.com>
2024-09-25 14:39:34 +0530

    working on group page content and responsive 1

--- pace-website/src/app/about/page.jsx
+++ pace-website/src/app/about/page.jsx
@@ -12,7 +12,7 @@ export default function About() {
     <>
       <PageOcc />
       <Hero2 />
-      {/* <CompanyProfile /> */}
+      <CompanyProfile />
       <AboutCards />
       {/* <Zigzag
         styles={{
