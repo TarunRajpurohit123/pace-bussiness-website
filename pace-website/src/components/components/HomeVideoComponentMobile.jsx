74ee69bf7761487ecdefed201694e3ef360148af
Tarun Rajpurohit <87821489+TarunRajpurohit123@users.noreply.github.com>
2024-09-25 22:10:08 +0530

    set demo banner to home page

--- pace-website/src/components/components/HomeVideoComponentMobile.jsx
+++ pace-website/src/components/components/HomeVideoComponentMobile.jsx
@@ -1,17 +1,34 @@
+import Image from "next/image";
+
 export default function HomeVideoComponentMobile() {
   return (
     <>
-      <div className="video-background--mobile">
-        <video autoPlay loop muted className="video">
-          <source src="/vid-bg.mp4" type="video/mp4" />
-          <source src="/vid-bg.ogg" type="video/ogg" />
-          Your browser does not support the video tag.
-        </video>
-        {/* <div className="content"> */}
-        {/* Your content goes here */}
-        {/* <h1>Welcome to Our Website</h1> */}
-        {/* </div> */}
+      <div className="temp--home--banner">
+        <Image
+          src="/main/home/tempbanner.jpg"
+          width={640}
+          height={960}
+          alt="home banner"
+        />
       </div>
     </>
   );
 }
+
+// export default function HomeVideoComponentMobile() {
+//   return (
+//     <>
+//       <div className="video-background--mobile">
+//         <video autoPlay loop muted className="video">
+//           <source src="/vid-bg.mp4" type="video/mp4" />
+//           <source src="/vid-bg.ogg" type="video/ogg" />
+//           Your browser does not support the video tag.
+//         </video>
+//         {/* <div className="content"> */}
+//         {/* Your content goes here */}
+//         {/* <h1>Welcome to Our Website</h1> */}
+//         {/* </div> */}
+//       </div>
+//     </>
+//   );
+// }
