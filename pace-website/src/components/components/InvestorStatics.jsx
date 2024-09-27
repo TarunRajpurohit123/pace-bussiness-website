f351ce2c0fdc70bbef23dea9a900ded0966575aa
Tarun Rajpurohit <87821489+TarunRajpurohit123@users.noreply.github.com>
2024-09-27 13:21:37 +0530

    investor statics update

--- pace-website/src/components/components/InvestorStatics.jsx
+++ pace-website/src/components/components/InvestorStatics.jsx
@@ -3,12 +3,12 @@ export default function InvestorStatics() {
     <section className="pt-[7.5rem] pb-[7.5rem] InvestorStatics--mobile">
       <section className="page-width flex justify-between investor__statics items-center gradient-border-container">
         <div className="investor_state_card">
-          <h1 className="investor_state_heading">₹2,837.16 lacs.</h1>
-          <p className="investor_state_span">Revenue 2023</p>
+          <h1 className="investor_state_heading">₹4281.41 lacs</h1>
+          <p className="investor_state_span">Revenue 2023-24</p>
         </div>
         <div className="investor_state_card">
-          <h1 className="investor_state_heading">₹62.25 lacs.</h1>
-          <p className="investor_state_span">Net Profit, Group Share 2023</p>
+          <h1 className="investor_state_heading">₹239.28 lacs</h1>
+          <p className="investor_state_span">Net Profit, Group Share 2023-24</p>
         </div>
       </section>
     </section>
