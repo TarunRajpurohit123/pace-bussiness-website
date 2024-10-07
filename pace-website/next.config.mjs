ee3f3fb8763e1ae46302fdd87fd656d1c6a38252
Tarun Rajpurohit <87821489+TarunRajpurohit123@users.noreply.github.com>
2024-10-08 00:37:17 +0530

    done

--- pace-website/next.config.mjs
+++ pace-website/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  basePath: "/test/out",
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
